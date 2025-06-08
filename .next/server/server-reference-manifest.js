264c3be1a4fae31c6890927be24b5e167f8b25f8
Gerardo Díaz Mandujano <gerardodiman@gmail.com>
2025-06-08 04:08:45 +0000

    Reinstalacion

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"deHoK6oQ86FsvExSaMXD+ynaz+GYjigTkmizIiKg0Yw=\"\n}"
---
+++ .next/static/chunks/src_app_page_tsx_bd85fe0d._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_page_tsx_bd85fe0d._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_page_tsx_bd85fe0d._.js",
+  "chunks": [
+    "static/chunks/src_e79b19fd._.js",
+    "static/chunks/node_modules_0c101a62._.js"
+  ],
+  "source": "dynamic"
+});
